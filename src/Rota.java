86d0613abd91e2e552ed251dce2e4dd23eda2881
melpereira7 <melanipereira98@gmail.com>
2021-12-05 22:15:00 +0000

    Classe Rota

---
+++ src/Rota.java
@@ -0,0 +1,100 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Rota {
+    private InetAddress servidor;
+    private int porta_servidor;
+    private String origem;
+    private int saltos;
+    private List<String> destinos = new ArrayList<>(); //vizinhos?
+    private int estado; //0 - inativa; 1 - ativa
+
+    /*
+    identificação do servidor, identificação do fluxo, valor da métrica, etc;
+    cada nó deve atualizar a tabela de rotas com: Servidor/Fluxo, Origem, Métrica, Destinos, Estado da Rota
+
+     */
+
+    public Rota(InetAddress servidor, int porta_servidor, String origem, int saltos, List<String> destinos){
+        this.servidor = servidor;
+        this.porta_servidor = porta_servidor;
+        this.origem = origem;
+        this.saltos = saltos;
+        this.destinos = destinos;
+        this.estado = 0;
+    }
+
+    public InetAddress getServidor() {
+        return servidor;
+    }
+
+    public void setServidor(InetAddress servidor) {
+        this.servidor = servidor;
+    }
+
+    public int getPorta_servidor() {
+        return porta_servidor;
+    }
+
+    public void setPorta_servidor(int porta_servidor) {
+        this.porta_servidor = porta_servidor;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public int getSaltos() {
+        return saltos;
+    }
+
+    public void setSaltos(int saltos) {
+        this.saltos = saltos;
+    }
+
+    public List<String> getDestinos() {
+        return destinos;
+    }
+
+    public void setDestinos(List<String> destinos) {
+        this.destinos = destinos;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public void activate() {
+        this.estado = 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Rota{" +
+                "servidor=" + servidor +
+                ", porta_servidor=" + porta_servidor +
+                ", origem='" + origem + '\'' +
+                ", saltos=" + saltos +
+                ", destinos=" + destinos +
+                ", estado=" + estado +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rota rota = (Rota) o;
+        return porta_servidor == rota.porta_servidor && saltos == rota.saltos && estado == rota.estado && servidor.equals(rota.servidor) && origem.equals(rota.origem) && destinos.equals(rota.destinos);
+    }
+}
--- src/Router.java
+++ src/Router.java
@@ -1,32 +1,75 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+   enunciado resumido:
+       servidor vai enviar mensagem de controlo para criar rotas - inclui identificação do servidor, identificação do fluxo, valor da métrica, etc;
+       cada nó deve atualizar a tabela de rotas com: Servidor/Fluxo, Origem, Métrica, Destinos, Estado da Rota;
+       todos os nós vão difundir a mensagem com o valor da métrica atualizado (+1 salto) para todos os vizinhos menos de onde a receberam;
+       estado da rota é inicialmente inativo
+       são ativadas pelo cliente através de uma mensagem de ativação de rota pelo percurso inverso
+       cada nó deve reencaminhar uma mensagem de pedido de ativação seguindo o campo "origem" da rota
+
+   o que acho que devemos fazer:
+       servidor envia mensagem para nó1
+       nó1 regista rota para o servidor
+       nó1 envia mensagem para nó2 que indica que se quer ir ao servidor é para ele que envia e que o valor da métrica é 1
+       nó2 repete o que nó1 fez mas adiciona 1 à métrica
+       e assim sucessivamente
+       quando um cliente inicia e quiser pedir dados ao servidor deve enviar uma mensgaem de pedido de rota aos seus vizinhos
+       cada vizinho deve responder com "esta é a rota que eu tenho e tem este valor de saltos"
+       o cliente decide qual a melhor de acordo com o menor nº de saltos
+       e envia essa informação ao vizinho selecionado que adiciona o cliente à rota e ativa a rota em questão enviando ainda mensagem ao servidor com o pedido de dados
+   */
 
 public class Router {
-    private Map<String,String> routing_table = new HashMap<>(); //string,string -> ipadress destino, ipadress prox salto
+    private List<String> vizinhos;
+    private List<Rota> routing_table;
+    private DatagramSocket socket;
 
-    public Router(){
+    public Router() {
+        this.routing_table = new ArrayList<>();
+    }
+
+    public void addRoute(InetAddress servidor, int porta_servidor, String origem, int saltos){
+        this.routing_table.add(new Rota(servidor,porta_servidor,origem,saltos,vizinhos));
+    }
+
+    public void addRoute(Rota rota){
+        this.routing_table.add(rota);
+    }
+
+    public void run(){
+        new Thread(() -> {
+            while (true){
+                byte[] messageReceived = new byte[512];
+                DatagramPacket pacote = new DatagramPacket(messageReceived,512);
+
+                try {
+                    socket.receive(pacote);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String data = Arrays.toString(pacote.getData());
+                //dependendo do formato da mensagem, tratar os dados e adicionar rota
+
+                /* reencaminhar a mensagem para todos os vizinhos, não esquecer adicionar um salto à métrica
+                try {
+                    socket.send(new DatagramPacket());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        }).start();
 
     }
 
-    /*
-    enunciado resumido:
-        servidor vai enviar mensagem de controlo para criar rotas - inclui identificação do servidor, identificação do fluxo, valor da métrica, etc;
-        cada nó deve atualizar a tabela de rotas com: Servidor/Fluxo, Origem, Métrica, Destinos, Estado da Rota;
-        todos os nós vão difundir a mensagem com o valor da métrica atualizado (+1 salto) para todos os vizinhos menos de onde a receberam;
-        estado da rota é inicialmente inativo
-        são ativadas pelo cliente através de uma mensagem de ativação de rota pelo percurso inverso
-        cada nó deve reencaminhar uma mensagem de pedido de ativação seguindo o campo "origem" da rota
 
-    o que acho que devemos fazer:
-        servidor envia mensagem para nó1
-        nó1 regista rota para o servidor
-        nó1 envia mensagem para nó2 que indica que se quer ir ao servidor é para ele que envia e que o valor da métrica é 1
-        nó2 repete o que nó1 fez mas adiciona 1 à métrica
-        e assim sucessivamente
-        quando um cliente inicia e quiser pedir dados ao servidor deve enviar uma mensgaem de pedido de rota aos seus vizinhos
-        cada vizinho deve responder com "esta é a rota que eu tenho e tem este valor de saltos"
-        o cliente decide qual a melhor de acordo com o menor nº de saltos
-        e envia essa informação ao vizinho selecionado que adiciona o cliente à rota e ativa a rota em questão enviando ainda mensagem ao servidor com o pedido de dados
-    */
 
 }
